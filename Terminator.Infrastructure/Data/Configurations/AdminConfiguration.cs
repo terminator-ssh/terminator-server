d7f56b34dd1a0b2e0e9353a8d6bdfaa8ce15b723
Deeplerg <47917846+Deeplerg@users.noreply.github.com>
2025-12-26 13:42:37 +0500

    Add missing AdminConfiguration.cs

---
+++ Terminator.Infrastructure/Data/Configurations/AdminConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Terminator.Core.Common;
+using Terminator.Core.Entities;
+
+namespace Terminator.Infrastructure.Data.Configurations;
+
+public class AdminConfiguration : IEntityTypeConfiguration<Admin>
+{
+    public void Configure(EntityTypeBuilder<Admin> builder)
+    {
+        builder.HasKey(u => u.Id);
+        
+        builder
+            .Property(u => u.Username)
+            .IsRequired()
+            .HasMaxLength(AdminConstants.MaxUsernameLength);
+        
+        builder
+            .HasIndex(u => u.Username)
+            .IsUnique();
+        
+        builder
+            .Property(u => u.PasswordHash)
+            .IsRequired()
+            .HasMaxLength(AdminConstants.MaxUsernameLength);
+    }
+}
